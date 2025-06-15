3099afe89f9be78a808e43adf0b5e68f227762c4
sophtang <82526853+sophtang@users.noreply.github.com>
2025-06-15 15:09:10 +0000

    Deploying to gh-pages from @ sophtang/sophtang.github.io@45e9fe1ce3769ef2e72924f8b7304f0f14f743fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,16 +23,17 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/articles/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+        },{id: "news-gumbel-softmax-flow-matching-is-out-on-arxiv-check-out-our-work-on-guided-discrete-flow-matching-on-the-simplex",
+          title: 'Gumbel-Softmax Flow Matching is out on arXiv! Check out our work on guided...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-my-review-paper-on-peptide-functionalized-nanoparticles-for-brain-targeted-therapeutics-is-published-in-drug-delivery-and-translational-research",
+          title: 'My review paper on Peptide-Functionalized Nanoparticles for Brain-Targeted Therapeutics is published in Drug...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-peptune-de-novo-generation-of-therapeutic-peptides-with-multi-objective-guided-discrete-diffusion-accepted-at-icml-2025-come-by-us-at-poster-session-4-to-chat-free-peptune-stickers",
+          title: 'PepTune: De Novo Generation of Therapeutic Peptides with Multi-Objective-Guided Discrete Diffusion accepted at...',
+          description: "",
+          section: "News",},{id: "news-branched-schrödinger-bridge-matching-released-on-arxiv-and-accepted-at-icml-exait-workshop-come-by-our-poster-in-vancouver",
+          title: 'Branched Schrödinger Bridge Matching released on arXiv and accepted at ICML ExAIT workshop!...',
           description: "",
           section: "News",},{
       id: 'light-theme',
