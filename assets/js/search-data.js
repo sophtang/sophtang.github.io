25b732cc3262f4554b6b424b3f48d1c93a5f8fa6
sophtang <82526853+sophtang@users.noreply.github.com>
2025-07-20 18:47:04 +0000

    Deploying to gh-pages from @ sophtang/sophtang.github.io@da172aa7ddf30c8fee58832b64de1af146b9a9e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "Publications",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "nav-technical-articles",
+  },{id: "nav-technical-articles",
           title: "Technical Articles",
           description: "",
           section: "Navigation",
